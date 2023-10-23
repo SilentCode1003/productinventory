384722492150e7571b149853409b507beb64c183
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-10-23 16:14:15 +0800

    Added model class and db

---
+++ routes/model/model.js
@@ -0,0 +1,96 @@
+class MasterItemModel {
+  constructor(id, name, category, status, createdby, createddate) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.status = status;
+    this.createdby = createdby;
+    this.createddate = createddate;
+  }
+}
+
+class MasterCategoryModel {
+  constructor(id, name, status, createdby, createddate) {
+    this.id = id;
+    this.name = name;
+    this.status = status;
+    this.createdby = createdby;
+    this.createddate = createddate;
+  }
+}
+
+class EmployeeModel {
+  constructor(
+    id,
+    fullname,
+    position,
+    department,
+    username,
+    password,
+    access,
+    status,
+    createdby,
+    createddate
+  ) {
+    this.id = id;
+    this.fullname = fullname;
+    this.position = position;
+    this.department = department;
+    this.username = username;
+    this.password = password;
+    this.access = access;
+    this.status = status;
+    this.createdby = createdby;
+    this.createddate = createddate;
+  }
+}
+
+class ProductModel {
+  constructor(serial, category, podate, ponumber, warrantydate, status) {
+    this.serial = serial;
+    this.category = category;
+    this.podate = podate;
+    this.ponumber = ponumber;
+    this.warrantydate = warrantydate;
+    this.status = status;
+  }
+}
+
+class DeployModel {
+  constructor(id, serial, date, deployby, deployto) {
+    this.id = id;
+    this.serial = serial;
+    this.date = date;
+    this.deployby = deployby;
+    this.deployto = deployto;
+  }
+}
+
+class ReturnModel {
+  constructor(id, serial, date, returnby, returnfrom) {
+    this.id = id;
+    this.serial = serial;
+    this.date = date;
+    this.returnby = returnby;
+    this.returnfrom = returnfrom;
+  }
+}
+
+class RepairModel {
+  constructor(id, serial, date, repairby) {
+    this.id = id;
+    this.serial = serial;
+    this.date = date;
+    this.repairby = repairby;
+  }
+}
+
+module.exports = {
+  MasterItemModel,
+  MasterCategoryModel,
+  EmployeeModel,
+  ProductModel,
+  DeployModel,
+  ReturnModel,
+  RepairModel
+};
---
+++ routes/model/spimodel.js
@@ -0,0 +1,189 @@
+const {
+  MasterItemModel,
+  MasterCategoryModel,
+  EmployeeModel,
+  ProductModel,
+  DeployModel,
+  ReturnModel,
+} = require("./model");
+
+exports.MasterItem = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      id: key.mi_id,
+      name: key.mi_name,
+      category: key.mi_category,
+      status: key.mi_status,
+      createdby: key.mi_createdby,
+      createddate: key.mi_createddate,
+    });
+  });
+
+  return dataResult.map(
+    (key) =>
+      new MasterItemModel(
+        key["id"],
+        key["name"],
+        key["category"],
+        key["status"],
+        key["createdby"],
+        key["createddate"]
+      )
+  );
+};
+
+exports.MasterItem = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      id: key.mc_id,
+      inamed: key.mc_name,
+      status: key.mc_status,
+      createdby: key.mc_createdby,
+      createddate: key.mc_createddate,
+    });
+  });
+
+  return dataResult.map(
+    (key) =>
+      new MasterCategoryModel(
+        key["id"],
+        key["name"],
+        key["status"],
+        key["createdby"],
+        key["createddate"]
+      )
+  );
+};
+
+exports.Employee = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      id: key.e_id,
+      fullname: key.e_fullname,
+      position: key.e_position,
+      department: key.e_department,
+      username: key.e_username,
+      password: key.e_password,
+      access: key.e_access,
+      status: key.e_status,
+      createdby: key.e_createdby,
+      createddate: key.e_createddate,
+    });
+  });
+
+  return dataResult.map(
+    (key) =>
+      new EmployeeModel(
+        key["id"],
+        key["fullname"],
+        key["position"],
+        key["department"],
+        key["username"],
+        key["password"],
+        key["status"],
+        key["createdby"],
+        key["createddate"]
+      )
+  );
+};
+
+exports.Product = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      serial: key.p_serial,
+      category: key.p_category,
+      podate: key.p_podate,
+      ponumber: key.p_ponumber,
+      warrantydate: key.p_warrantydate,
+      status: key.p_status,
+    });
+  });
+
+  return dataResult.map(
+    (key) =>
+      new ProductModel(
+        key["serial"],
+        key["category"],
+        key["podate"],
+        key["ponumber"],
+        key["warrantydate"],
+        key["status"]
+      )
+  );
+};
+
+exports.Deploy = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      id: key.d_id,
+      serial: key.d_serial,
+      date: key.d_date,
+      deployby: key.d_deployby,
+      deployto: key.d_deployto,
+    });
+  });
+
+  return dataResult.map(
+    (key) =>
+      new DeployModel(
+        key["id"],
+        key["serial"],
+        key["date"],
+        key["deployby"],
+        key["deployto"]
+      )
+  );
+};
+
+exports.Return = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      id: key.r_id,
+      serial: key.r_serial,
+      date: key.r_date,
+      returnby: key.r_returnby,
+      returnfrom: key.r_returnfrom,
+    });
+  });
+
+  return dataResult.map(
+    (key) =>
+      new ReturnModel(
+        key["id"],
+        key["serial"],
+        key["date"],
+        key["returnby"],
+        key["returnfrom"]
+      )
+  );
+};
+
+exports.Return = (data) => {
+  let dataResult = [];
+
+  data.forEach((key, item) => {
+    dataResult.push({
+      id: key.r_id,
+      serial: key.r_serial,
+      date: key.r_date,
+      repairby: key.r_repairby,
+    });
+  });
+
+  return dataResult.map(
+    (key) =>
+      new ReturnModel(key["id"], key["serial"], key["date"], key["r_repairby"])
+  );
+};
