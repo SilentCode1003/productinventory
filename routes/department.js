64fbbb5e46bb7d592500e8c3eebf98af0a5b83d6
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-12-05 11:54:14 +0800

    Added Department and Product save API

--- routes/department.js
+++ routes/department.js
@@ -1,9 +1,140 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const { Update, Select, InsertTable } = require("./repository/spidb");
+const dictionary = require("./repository/dictionary");
+const helper = require("./repository/customhelper");
+const { MasterDepartment } = require("./model/spimodel");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('department', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("department", { title: "Express" });
 });
 
 module.exports = router;
+
+router.get("/load", (req, res) => {
+  try {
+    let sql = `select * from master_department`;
+
+    Select(sql, (err, result) => {
+      if (err) console.error("Error: ", err);
+
+      console.log(result);
+
+      if (result.length != 0) {
+        let data = MasterDepartment(result);
+
+        console.log(data);
+        res.json({
+          msg: "success",
+          data: data,
+        });
+      } else {
+        res.json({
+          msg: "success",
+          data: result,
+        });
+      }
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
+router.post("/save", (req, res) => {
+  try {
+    let name = req.body.name;
+    let status = dictionary.GetValue(dictionary.ACT());
+    let createdby =
+      req.session.fullname == null ? "dev42" : req.session.fullname;
+    let createddate = helper.GetCurrentDatetime();
+    let master_department = [];
+
+    master_department.push([name, status, createdby, createddate]);
+    InsertTable("master_department", master_department, (err, result) => {
+      if (err) console.error("Error: ", err);
+
+      console.log(result);
+      res.json({
+        msg: "success",
+      });
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
+router.post("/edit", (req, res) => {
+  try {
+    let namemodal = req.body.namemodal;
+    let id = req.body.id;
+
+    let data = [namemodal, id];
+
+    let sql_Update = `UPDATE master_department 
+                     SET md_name = ?
+                     WHERE md_id = ?`;
+
+    let sql_check = `SELECT * FROM master_department WHERE md_id='${id}'`;
+
+    console.log(data);
+
+    Select(sql_check, (err, result) => {
+      if (err) console.error("Error: ", err);
+
+      if (result.length != 1) {
+        return res.json({
+          msg: "notexist",
+        });
+      } else {
+        Update(sql_Update, data, (err, result) => {
+          if (err) console.error("Error: ", err);
+
+          console.log(result);
+
+          res.json({
+            msg: "success",
+          });
+        });
+      }
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
+router.post("/status", (req, res) => {
+  try {
+    let id = req.body.id;
+    let status =
+      req.body.status == dictionary.GetValue(dictionary.ACT())
+        ? dictionary.GetValue(dictionary.INACT())
+        : dictionary.GetValue(dictionary.ACT());
+    let data = [status, id];
+
+    let sql_Update = `UPDATE master_department 
+                     SET md_status = ?
+                     WHERE md_id = ?`;
+
+    console.log(data);
+
+    Update(sql_Update, data, (err, result) => {
+      if (err) console.error("Error: ", err);
+
+      res.json({
+        msg: "success",
+      });
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
