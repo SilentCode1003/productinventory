7726b04bc96173d7f52c40e93440ce7e1a2abed0
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-12-15 09:35:19 +0800

    Fix sold API

--- routes/sold.js
+++ routes/sold.js
@@ -1,14 +1,15 @@
 var express = require("express");
 const { Sold } = require("./model/spimodel");
-const { InsertTable } = require("./repository/spidb");
+const { InsertTable, Select, Update } = require("./repository/spidb");
 const { SLD, GetValue } = require("./repository/dictionary");
 const { Validator } = require("./controller/middleware");
+const { SelectStatement } = require("./repository/customhelper");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   // res.render("sold", { title: "Express" });
-  Validator(req, res, "sold")
+  Validator(req, res, "sold");
 });
 
 module.exports = router;
@@ -29,11 +30,20 @@ router.get("/load", (req, res) => {
     Select(sql, (err, result) => {
       if (err) console.error("Error: ", err);
 
-      let data = Sold(result);
-      res.json({
-        msg: "success",
-        data: data,
-      });
+      if (result.length != 0) {
+        let data = Sold(result);
+
+        console.log(data);
+        res.json({
+          msg: "success",
+          data: data,
+        });
+      } else {
+        res.json({
+          msg: "success",
+          data: result,
+        });
+      }
     });
   } catch (error) {
     res.json({
@@ -47,7 +57,7 @@ router.post("/save", (req, res) => {
     const { assetcontrol, serial, date, soldby, soldto, referenceno } =
       req.body;
     let sold = [[assetcontrol, serial, date, soldby, soldto, referenceno]];
-    console.log("Sold data: ", sold)
+    console.log("Sold data: ", sold);
     Check_Sold(assetcontrol, date, soldto)
       .then((result) => {
         let data = Sold(result);
