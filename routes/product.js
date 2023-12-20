fa4dd2cf8eed6faf53a82a59749636f015cb3cc7
Rhaz05 <laurenrodrigueza9@gmail.com>
2023-12-20 16:31:33 +0800

    Added display for current stocks in dashboard

--- routes/product.js
+++ routes/product.js
@@ -77,6 +77,51 @@ router.get("/load", (req, res) => {
   }
 });
 
+router.get("/getstocks", (req, res) => {
+  try {
+    let sql = `
+      SELECT 
+        p.p_assetcontrol as p_assetcontrol,
+        p.p_serial as p_serial,
+        mi.mi_name as p_itemname,
+        mc.mc_name as p_category,
+        p.p_podate as p_podate,
+        p.p_ponumber as p_ponumber,
+        p.p_warrantydate as p_warrantydate,
+        p.p_status as p_status
+      FROM 
+        product p
+      INNER JOIN 
+        master_item mi ON p.p_itemname = mi.mi_id
+      INNER JOIN 
+        master_category mc ON p.p_category = mc.mc_id
+      WHERE p_status in ('WAREHOUSE', 'RETURNED');`;
+
+    Select(sql, (err, result) => {
+      if (err) console.error("Error: ", err);
+
+      if (result.length != 0) {
+        let data = Product(result);
+
+        // console.log(data);
+        res.json({
+          msg: "success",
+          data: data,
+        });
+      } else {
+        res.json({
+          msg: "success",
+          data: result,
+        });
+      }
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
 router.get("/loadproduct", (req, res) => {
   try {
     const page = req.query.page || 1;
