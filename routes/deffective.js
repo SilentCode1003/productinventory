f9c53b8372925a0c106711b2919b8c0395ecdb51
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-12-28 16:27:43 +0800

    Added save deffective API and fix replace API

--- routes/deffective.js
+++ routes/deffective.js
@@ -1,5 +1,10 @@
 var express = require("express");
-const { Repair, Product, RepairProduct, DeffectiveItem } = require("./model/spimodel");
+const {
+  Repair,
+  Product,
+  RepairProduct,
+  DeffectiveItem,
+} = require("./model/spimodel");
 const {
   Select,
   InsertTable,
@@ -12,7 +17,11 @@ const {
 } = require("./repository/customhelper");
 const { GetValue, RPRD } = require("./repository/dictionary");
 const { Validator } = require("./controller/middleware");
-const { JsonErrorResponse } = require("./repository/responce");
+const {
+  JsonErrorResponse,
+  JsonWarningResponse,
+  JsonSuccess,
+} = require("./repository/responce");
 var router = express.Router();
 
 /* GET home page. */
@@ -52,3 +61,49 @@ router.get("/load", (req, res) => {
     res.json(JsonErrorResponse());
   }
 });
+
+router.post("/save", (req, res) => {
+  try {
+    const { assetcontrol, itemserial, remarks, date, referenceno } = req.body;
+
+    Check_DeffectiveItem(assetcontrol, date)
+      .then((result) => {
+        let data = DeffectiveItem(result);
+
+        if (data.length != 0) {
+          res.json(JsonWarningResponse("exist"));
+        } else {
+          let deffectiveitem = [
+            [assetcontrol, itemserial, remarks, date, referenceno],
+          ];
+
+          InsertTable("deffectiveitem", deffectiveitem, (err, result) => {
+            if (err) console.error("Error: ", err);
+            console.log(result);
+            res.json(JsonSuccess());
+          });
+        }
+      })
+      .catch((error) => {
+        res.json(JsonErrorResponse(error));
+      });
+  } catch (error) {
+    res.json(JsonErrorResponse(error));
+  }
+});
+
+//#region Function
+function Check_DeffectiveItem(assetcontrol, date) {
+  return new Promise((resolve, reject) => {
+    let sql =
+      "select * from deffectiveitem where d_assetcontrol=? and d_date=?";
+    let commad = SelectStatement(sql, [assetcontrol, date]);
+
+    Select(commad, (err, result) => {
+      if (err) reject(err);
+
+      resolve(result);
+    });
+  });
+}
+//#endregion
