fc7c9a6b0408770d56f2d1d0b877d86600044761
Rhaz05 <laurenrodrigueza9@gmail.com>
2024-01-22 10:17:11 +0800

    [sold-report]-[Added deliveryfee(Back End) and updated sold scripts(Front End)]-[/save]

--- routes/sold.js
+++ routes/sold.js
@@ -81,7 +81,7 @@ router.get("/load", (req, res) => {
 
 router.post("/save", (req, res) => {
   try {
-    const { assetcontrol, serial, date, soldby, soldto, referenceno, paymenttype, sellingprice, remarks, transactionstatus, transactionref } =
+    const { assetcontrol, serial, date, soldby, soldto, referenceno, paymenttype, sellingprice, deliveryfee, remarks, transactionstatus, transactionref } =
       req.body;
     let sold = [[assetcontrol, serial, date, soldby, soldto, referenceno]];
     // console.log("Sold data: ", sold);
@@ -92,7 +92,7 @@ router.post("/save", (req, res) => {
         let quantity = 1;
         let category = product[0].category;
         let itemname = product[0].itemname;
-        let salesreport = [[category, itemname, date, quantity, sellingprice, soldby, soldto, paymenttype, referenceno, transactionref, remarks, transactionstatus]]
+        let salesreport = [[category, itemname, date, quantity, sellingprice, deliveryfee, soldby, soldto, paymenttype, referenceno, transactionref, remarks, transactionstatus]]
 
         InsertTable("sales_report", salesreport, (err, result) => {
           if (err) console.error("Error: ", err);
