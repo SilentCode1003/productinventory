7d3efb3e8bc5e4c8152dd3a6b4deff51579d6f7c
Rhaz05 <laurenrodrigueza9@gmail.com>
2024-07-08 10:24:58 +0800

    PDF Update
    Additional: -Sold Products Report can now be exported to PDF

--- routes/repository/pdf.js
+++ routes/repository/pdf.js
@@ -1,29 +1,29 @@
-const PdfMake = require("pdfmake");
-const fs = require("fs");
-const { document } = require("./pdfcontent");
-const path = require("path");
-const { Template } = require("ejs");
-require("dotenv").config();
+const PdfMake = require('pdfmake')
+const fs = require('fs')
+const { document } = require('./pdfcontent')
+const path = require('path')
+const { Template } = require('ejs')
+require('dotenv').config()
 
-const regularfont = path.join(__dirname, "/fonts/roboto-regular-webfont.ttf");
-const boldfont = path.join(__dirname, "/fonts/roboto-bold-webfont.ttf");
+const regularfont = path.join(__dirname, '/fonts/roboto-regular-webfont.ttf')
+const boldfont = path.join(__dirname, '/fonts/roboto-bold-webfont.ttf')
 
 exports.Generate = (data, template, employee, date) => {
   // console.log("Generating Phase: ", data);
 
   return new Promise((resolve, reject) => {
-    var fonts = {
+    const fonts = {
       Roboto: {
         normal: regularfont,
         bold: boldfont,
         italics: regularfont,
         bolditalics: regularfont,
       },
-    };
+    }
 
-    const printer = new PdfMake(fonts);
+    const printer = new PdfMake(fonts)
 
-    var reportContent = document(data, template, employee, date);
+    const reportContent = document(data, template, employee, date)
 
     // console.log("Content: ", reportContent)
     // const pdfPath = path.join(
@@ -31,14 +31,14 @@ exports.Generate = (data, template, employee, date) => {
     //   `/reports/Sales_Report_${GetCurrentDate()}.pdf`
     // );
 
-    var pdfDoc = printer.createPdfKitDocument(reportContent);
+    const pdfDoc = printer.createPdfKitDocument(reportContent)
     // pdfDoc.pipe(fs.createWriteStream(pdfPath));
 
-    const chunks = [];
-    pdfDoc.on("data", (chunk) => chunks.push(chunk));
-    pdfDoc.on("end", () => resolve(Buffer.concat(chunks)));
-    pdfDoc.on("error", (error) => reject(error));
+    const chunks = []
+    pdfDoc.on('data', (chunk) => chunks.push(chunk))
+    pdfDoc.on('end', () => resolve(Buffer.concat(chunks)))
+    pdfDoc.on('error', (error) => reject(error))
 
-    pdfDoc.end();
-  });
-};
+    pdfDoc.end()
+  })
+}
